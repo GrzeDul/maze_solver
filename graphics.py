8266bc7fa5cb13ea4ed31a91468dc95b3e9734c9
Grzegorz Staszczak <grzestamail@gmail.com>
2024-07-12 16:16:12 +0200

    Init project

---
+++ graphics.py
@@ -0,0 +1,74 @@
+from tkinter import Tk, BOTH, Canvas
+
+class Window:
+    def __init__(self, width, height):
+        self.__root = Tk()
+        self.__root.title("Maze Solver")
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+        self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
+        self.__canvas.pack(fill=BOTH, expand=1)
+        self.__running = False
+    
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+
+    def wait_for_close(self):
+        self.__running = True
+        while self.__running:
+            self.redraw()
+        print("window closed...")
+    
+    def draw_line(self, line, fill_color="black"):
+        line.draw(self.__canvas, fill_color)
+    
+    def close(self):
+        self.running = False
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class Line:
+    def __init__(
+        self,
+        p1,
+        p2,
+    ):
+        self.p1 = p1
+        self.p2 = p2
+
+    def draw(self, canvas, fill_color="black"):
+        canvas.create_line(
+            self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2
+        )
+
+class Cell:
+  
+    def __init__(self, x1, y1, x2, y2, win):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        # top left and bottom right corners
+        self._x1 = x1
+        self._x2 = x2
+        self._y1 = y1
+        self._y2 = y2
+        self._win = win
+
+    def draw(self):
+        if self.has_left_wall:
+            line = Line(Point(self._x1, self._y1), Point(self._x1, self._y2))
+            self._win.draw_line(line)
+        if self.has_right_wall:
+            line = Line(Point(self._x2, self._y1), Point(self._x2, self._y2))
+            self._win.draw_line(line)
+        if self.has_top_wall:
+            line = Line(Point(self._x1, self._y1),Point(self._x2, self._y1))
+            self._win.draw_line(line)
+        if self.has_bottom_wall:
+            line = Line(Point(self._x1, self._y2),Point(self._x2, self._y2))
+            self._win.draw_line(line,)
---
+++ maze.py
@@ -0,0 +1,67 @@
+from cell import Cell
+import time, random
+class Maze:
+    def __init__(
+        self, 
+        x1,
+        y1,
+        num_rows,
+        num_cols,
+        cell_size_x,
+        cell_size_y,
+        win = None,
+        seed = None
+        ):
+        self._x1 = x1
+        self._y1 = y1
+        self._num_rows = num_rows
+        self._num_cols = num_cols
+        self._cell_size_x = cell_size_x
+        self._cell_size_y = cell_size_y
+        self._win = win
+        self._cells = []
+        self._create_cells()
+        self._break_entrence_and_exit()
+        random.seed(seed)
+        for i in range(self._num_cols):
+            for j in range(self._num_rows):
+                self._break_all_r(i,j)
+                
+    def _create_cells(self):
+        for col_num in range(self._num_cols):
+            row = []
+            for row_num in range(self._num_rows):
+                row.append(Cell(self._win))
+            self._cells.append(row)
+
+        for i in range(self._num_cols):
+            for j in range(self._num_rows):
+                self._draw_cell(i,j)
+                
+    
+    def _draw_cell(self, i, j):
+        if self._win is None:
+            return
+        x1 = self._x1 + i * self._cell_size_x
+        y1 = self._y1 + j * self._cell_size_y
+        x2 = x1 + self._cell_size_x
+        y2 = y1 + self._cell_size_y
+        self._cells[i][j].draw(x1, y1, x2, y2)
+        self._animate()
+    
+    def _animate(self):
+        if self._win is None:
+            return
+        self._win.redraw()
+        time.sleep(0.00)
+    
+    def _break_entrence_and_exit(self):
+        self._cells[0][0].has_left_wall = False
+        self._cells[self._num_cols-1][self._num_rows-1].has_right_wall = False
+        self._draw_cell(0,0)
+        self._draw_cell(self._num_cols-1, self._num_rows-1)
+    
+    def _break_all_r(self, i, j):
+        pass
+
+            
